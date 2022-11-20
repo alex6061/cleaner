4dfbd62f006c54672c728c4c67638a0f128401d9
alex6061 <39526012+alex6061@users.noreply.github.com>
2022-11-20 21:47:27 +0300

    scroll-menu

--- script.js
+++ script.js
@@ -19,6 +19,22 @@ $(document).ready (function() {
 
 
 
+window.onscroll = () =>  {
+    const header = document.querySelector ('.header');
+    // console.log(window.scrollY);
+
+    const Y = window.scrollY
+
+    if( Y > 2) {
+        // header = classList.add('header__fixed')
+        header.classList.add('header__fixed');
+        // console.log(window.scrollY)
+    }   else if (Y < 1) {
+        header.classList.remove('header__fixed');
+
+    }
+}
+
 
 
 
